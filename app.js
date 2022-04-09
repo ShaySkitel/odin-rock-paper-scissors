6af11e5c019ec47797b334317a57dccce7b1f20e
Shay Skitel <shay.skitel@gmail.com>
2022-04-09 04:06:49 +0300

    Create playRound function

--- app.js
+++ app.js
@@ -2,15 +2,53 @@
 function computerPlay() {
     switch (random(3)) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
 // Generate a random number up to but not including the given argument number
 function random(num) {
     return Math.floor(Math.random() * num);
+}
+
+// Play one round and return the winning or losing text
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+
+        if (playerSelection === "rock" && computerSelection === "rock") {
+            return `Draw! ${playerSelection} vs ${computerSelection}`;
+        }
+        else if (playerSelection === "rock" && computerSelection === "scissors") {
+            return `You Win! ${playerSelection} vs ${computerSelection}`;
+        }
+        else if (playerSelection === "rock" && computerSelection === "paper") {
+            return `You Lose! ${playerSelection} vs ${computerSelection}`;
+        }
+        else if (playerSelection === "paper" && computerSelection === "paper") {
+            return `Draw! ${playerSelection} vs ${computerSelection}`;
+        }
+        else if (playerSelection === "paper" && computerSelection === "rock") {
+            return `You Win! ${playerSelection} vs ${computerSelection}`;
+        }
+        else if (playerSelection === "paper" && computerSelection === "scissors") {
+            return `You Lose! ${playerSelection} vs ${computerSelection}`;
+        }
+        else if (playerSelection === "scissors" && computerSelection === "scissors") {
+            return `Draw! ${playerSelection} vs ${computerSelection}`;
+        }
+        else if (playerSelection === "scissors" && computerSelection === "paper") {
+            return `You Win! ${playerSelection} vs ${computerSelection}`;
+        }
+        else if (playerSelection === "scissors" && computerSelection === "rock") {
+            return `You Lose! ${playerSelection} vs ${computerSelection}`;
+        }
+    } else {
+        console.error("Invald Input. (rock, paper or scissors only)");
+        return false;
+    }
 }
